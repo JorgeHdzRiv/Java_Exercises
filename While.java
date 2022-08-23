31e441563e390e2262f977dfd16149be597ab779
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-08-23 15:25:42 -0500

    Ejemplo while

---
+++ While.java
@@ -0,0 +1,14 @@
+public class While {
+    public static void main(String[] args) {
+        int contador = 10;
+
+        while (contador > 0){
+            System.out.println("Contador : "+contador);
+            contador -= 1;
+            // Otra forma
+            // contador--;
+        }
+
+        System.out.println("Salimos del ciclo while");
+    }
+}
