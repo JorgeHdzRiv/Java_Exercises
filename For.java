e3542b0e69fd5db5d1481b0351acd54fb020b49f
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-08-23 16:01:41 -0500

    Ciclo For

---
+++ For.java
@@ -0,0 +1,13 @@
+public class For {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.println("El numero actual es: "+i);
+        }
+
+        int valores[]={1,2,3,4,5};
+
+        for (int j=0; j<valores.length; j++){
+            System.out.println(valores[j]);
+        }
+    }
+}
